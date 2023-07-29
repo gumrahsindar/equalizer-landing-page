8979464ade86a83396732627e8ffadca0828c70f
Gümrah Sindar <gsindar@gmail.com>
2023-07-29 20:30:55 +0300

    fix margins

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <section id='hero' className='px-6 md:px-10 pt-10 md:pt-16'>
+    <section id='hero' className='px-6 pt-10 md:pt-16 mb-48 md:px-10 md:mb-60'>
       <img src='/logo.svg' alt='' className='mb-16' />
       <h1 className='text-xl text-clr-black font-bold max-w-md md:text-3xl lg:text-4xl lg:max-w-3xl xl:max-w-4xl mb-5 md:mb-7 lg:mb-10'>
         We make your music sound extraordinary.
